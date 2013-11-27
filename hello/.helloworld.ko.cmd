3f0df2d50587bff73e4329135ac6096d0237abee
jason_yao <jason_yao@htc.com>
2013-11-27 21:16:14 +0800

    update

---
+++ hello/.helloworld.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/shihyu/data/LDD/hello/helloworld.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.5.0-43-generic/scripts/module-common.lds --build-id  -o /home/shihyu/data/LDD/hello/helloworld.ko /home/shihyu/data/LDD/hello/helloworld.o /home/shihyu/data/LDD/hello/helloworld.mod.o
---
+++ hello/.tmp_versions/helloworld.mod
@@ -0,0 +1,2 @@
+/home/shihyu/data/LDD/hello/helloworld.ko
+/home/shihyu/data/LDD/hello/helloworld.o
